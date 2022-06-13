9b551008d1aa0554b51fc1bc4b208394e3a35d1f
JesseSousa <rhadamesjesse14rj@gmail.com>
2022-06-13 20:44:11 -0300

    test user trigger method

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ const user = new User({ name: 'myName', age: 20 });
 
 user.set({ name: 'newName', age: 99 });
 
-user.on('change', () => {});
+user.on('change', () => {
+  console.log('change event handler was called');
+});
 
-console.log(user);
+user.trigger('change');
