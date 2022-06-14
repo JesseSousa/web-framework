ed0aab7495f25a8e5146a5b328afa00522a56f71
JesseSousa <rhadamesjesse14rj@gmail.com>
2022-06-13 21:30:31 -0300

    test save method

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,7 @@
 import { User } from './models/User';
 
-const user = new User({ name: 'myName', age: 20 });
+const user = new User({ name: 'John', age: 42 });
 
-user.set({ name: 'newName', age: 99 });
+user.save();
 
-user.on('change', () => {
-  console.log('change event handler was called');
-});
-
-user.trigger('change');
+console.log(user);
